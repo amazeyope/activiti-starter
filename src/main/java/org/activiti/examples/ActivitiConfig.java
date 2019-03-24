cd229a2ae9a3ec22d4fa1b9b1ba88492fae90e2c
ningyp@sunsharing.com.cn <abc123#2018>
2019-03-24 16:34:01 +0800

    5.22.0 版本starter 初始化提交

---
+++ src/main/java/org/activiti/examples/ActivitiConfig.java
@@ -0,0 +1,37 @@
+/*
+ * @(#) ActivitiConfig
+ * 版权声明 厦门畅享信息技术有限公司, 版权所有 违者必究
+ *
+ * <br> Copyright:  Copyright (c) 2019
+ * <br> Company:厦门畅享信息技术有限公司
+ * <br> @author ningyp
+ * <br> 2019-03-24 16:12:30
+ */
+
+package org.activiti.examples;
+
+import org.activiti.spring.SpringAsyncExecutor;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.activiti.spring.boot.AbstractProcessEngineAutoConfiguration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import javax.sql.DataSource;
+import java.io.IOException;
+/**
+ * activiti配置文件
+ * AbstractProcessEngineAutoConfiguration在activiti-spring-boot-starter-basic下
+ */
+@Configuration
+public class ActivitiConfig extends AbstractProcessEngineAutoConfiguration {
+    static final String NAME = "master";
+    //注入数据源和事务管理器
+    @Bean
+    public SpringProcessEngineConfiguration springProcessEngineConfiguration(
+            @Qualifier("dataSource") DataSource dataSource,
+            @Qualifier("transactionManager") PlatformTransactionManager transactionManager,
+            SpringAsyncExecutor springAsyncExecutor) throws IOException {
+        return this.baseSpringProcessEngineConfiguration(dataSource, transactionManager, springAsyncExecutor);
+    }
+}
